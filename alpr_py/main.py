f27c984d2d5d0240a2f14676485be46d32a76c81
hanbeul <han310@gmail.com>
2019-01-21 16:10:02 -0500

    Openalpr container correctly builds and runs. Needed to use an updated version of tesseract not found in Ubuntu repositories. Need to build custom hanbeul/openalpr image and use that as base image for lpdb image.

--- alpr_py/main.py
+++ alpr_py/main.py
@@ -3,6 +3,8 @@ import cv2
 from openalpr import Alpr
 from threading import Timer, Thread
 import json
+import locale
+locale.setlocale(locale.LC_ALL, 'C')
 
 scalefx = 1
 scalefy = 1
@@ -10,46 +12,50 @@ scalefy = 1
 sessions = {}
 source = './assets/vid_left_right_720p.mp4'
 
-alpr = Alpr('us', './openalpr/config/openalpr.conf.defaults', './openalpr/runtime_data/')
-
-iterations = 0
+alpr = Alpr('us', '/srv/openalpr/config/openalpr.conf.defaults', '/srv/openalpr/runtime_data/')
+if not alpr.is_loaded():
+  print("Error loading OpenALPR")
+  sys.exit(1)
 
 cap = cv2.VideoCapture(source)
-while(True):
-  iterations += 1
+
+#while(True):
+while(cap.isOpened()):
   ret, frame = cap.read()
   width = cap.get(3)
   height = cap.get(4)
 
   print('Height: ', height, 'Width: ', width)
-  print('Iteration: ', iterations)
+  print(alpr)
 
-  json = alpr.recognize_ndarray(frame)
-  
-  for result in json['results']:
+  json_results = alpr.recognize_ndarray(frame)
+
+  print('Json results: ', json_results)
+
+  for result in json_results['results']:
 
     plate = result['plate']
 
     print(plate, ' : ',  result['confidence'])
     
     # Draw rectangles around each license plate
-    x1 = result['coordinates'][0]['x']
-    y1 = result['coordinates'][0]['y']
-    x2 = result['coordinates'][2]['x']
-    y2 = result['coordinates'][2]['y']
+    #x1 = result['coordinates'][0]['x']
+    #y1 = result['coordinates'][0]['y']
+    #x2 = result['coordinates'][2]['x']
+    #y2 = result['coordinates'][2]['y']
 
-    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    #cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 10)
 
-    if plate in sessions: 
-      sessions[plate] += 2
-    else: 
-      sessions[plate] = 1
+    #if plate in sessions: 
+    #  sessions[plate] += 2
+    #else: 
+    #  sessions[plate] = 1
 
-  scale = cv2.resize(frame, None, fx=scalefx, fy=scalefy, interpolation = cv2.INTER_CUBIC)
-  cv2.imshow('frame', scale)
+  #scale = cv2.resize(frame, None, fx=scalefx, fy=scalefy, interpolation = cv2.INTER_CUBIC)
+  #cv2.imshow('frame', scale)
 
-  if cv2.waitKey(1) & 0xFF == ord('q'):
-    break
+  #if cv2.waitKey(1) & 0xFF == ord('q'):
+  #  break
 
 cap.release()
 alpr.unload()
