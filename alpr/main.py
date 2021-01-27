8ad91ee398b22e29caa354225d3c9952e4ec8cf2
hanbeul <han310@gmail.com>
2021-01-26 19:53:03 -0500

    Updated alpr code with example

--- alpr/main.py
+++ alpr/main.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "Hello World!"
+  return "Hello World from the ALPR container! :)"
+
+@app.route("/lpdb", methods=["GET", "POST"])
+def lpdb():
+  if request.method == "POST":
+    return True
+  else:
+    return False
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
