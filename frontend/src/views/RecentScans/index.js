27645d38b68a254fe8e61e67fa8fa4b3a88eebfc
mike-dt <kimyoungmichael2@gmail.com>
2021-05-28 18:38:46 -0400

    updated page arrows so that their color change when no more pages to turn to

--- frontend/src/views/RecentScans/index.js
+++ frontend/src/views/RecentScans/index.js
@@ -9,6 +9,8 @@ function RecentScans() {
     const [focus, setFocus] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [currentPagesVisits, setCurrentPagesVisits] = useState([]);
+    const [startIndex, setStartIndex] = useState([]);
+    const [endIndex, setEndIndex] = useState([]);
 
     useEffect(async () => {
         const res = await axios(
@@ -24,7 +26,9 @@ function RecentScans() {
         //the following useEffect updates it each time currentPage is updated. 
         //I feel like there would be a better way to do this...
         const startIndex = res.data.length - Number((currentPage - 1) + '1'); 
+        setStartIndex(startIndex);
         const endIndex = res.data.length - (currentPage * 10);
+        setEndIndex(endIndex);
         const selectedVisits = res.data.slice(endIndex, startIndex).reverse();
         console.log('this updates once during component mounting')
         setCurrentPagesVisits(selectedVisits);
@@ -32,19 +36,41 @@ function RecentScans() {
 
     useEffect(() => {
         const startIndex = visits.length - Number((currentPage - 1) + '1');
+        setStartIndex(startIndex);
         const endIndex = visits.length - (currentPage * 10);
+        setEndIndex(endIndex);
         const selectedVisits = visits.slice(endIndex, startIndex).reverse();
         console.log('this updates in the beginning, then whenever currentPage gets updated. It resolves sooner then above at start because above needs to fetch data')
         setCurrentPagesVisits(selectedVisits);
     },[currentPage])
 
     const handleLeftArrow = () => {
-        setCurrentPage(currentPage - 1);
+        //some of the mess below is to change the arrow colors when there is no more pages to turn to. 
+        if (currentPage == 1) { } //do nothing
+        else if (currentPage == 2) {
+            document.getElementById("leftArrow").classList.add("off");
+            setCurrentPage(currentPage - 1);
+        }
+        else {
+            document.getElementById("rightArrow").classList.remove("off");
+            setCurrentPage(currentPage - 1);
+        }
     }
 
     const handleRightArrow = () => {
-        setCurrentPage(currentPage + 1);
+        const lastPage = Math.round(visits.length/10);
+        if (currentPage <= 1) {
+            setCurrentPage(currentPage + 1);
+            document.getElementById("leftArrow").classList.remove("off")
+        }
+        else if (currentPage == lastPage -1) {
+            setCurrentPage(currentPage + 1)
+            document.getElementById("rightArrow").classList.add("off")
+        }
+        else if (currentPage >= lastPage) { }
+        else {setCurrentPage(currentPage + 1); console.log(currentPage + '  ' + lastPage)}
     }
+        
 
     return (
         <div className="recentScansPage">
@@ -52,9 +78,9 @@ function RecentScans() {
                 <FocusedScan />
             </div>
             <div className="pastTitle">
-                <div className="leftArrow" onClick={handleLeftArrow}>&lt;</div>
-                <Header>Scans #: 943-953</Header>
-                <div className="rightArrow" onClick={handleRightArrow}>&gt;</div>
+                <div id="leftArrow" className="leftArrow off" onClick={handleLeftArrow}>&lt;</div>
+                <Header>Scans #: {startIndex}-{endIndex}</Header>
+                <div id="rightArrow" className="rightArrow" onClick={handleRightArrow}>&gt;</div>
             </div>
             <div className="past">
                 <ScanList focus={focus} currentPage={currentPage} currentPagesVisits={currentPagesVisits}/>
