d5e2ea43e9c50eae171bbd411b73afc1a6f77ef8
hanbeul <han310@gmail.com>
2021-06-07 20:31:01 -0400

    Filter out data and send request to backend from alpr. Basic parsing in plate post handler

--- alpr/rtsp.py
+++ alpr/rtsp.py
@@ -5,6 +5,8 @@ load_dotenv()
 import os
 import ultimateAlprSdk
 import json
+import requests
+import time
 
 RTSP_PW = os.environ.get("RTSP_PW")
 RTSP_URL = "rtsp://lpdb:" + RTSP_PW + "@192.168.1.66/live"
@@ -52,6 +54,9 @@ ultimateAlprSdk.UltAlprSdkEngine_init(json.dumps(JSON_CONFIG))
 
 vcap = cv2.VideoCapture(RTSP_URL)
 
+recent_visits = {}
+last_plate = ""
+last_plate_count = 0
 
 while(1):
   ret, frame = vcap.read()
@@ -78,9 +83,27 @@ while(1):
     for x in result["plates"]:
       output.append(x["text"])
 
+  if "plates" in result.keys():
+    print("plate:", result["plates"][0]["text"], flush=True)
+    print("Recent visits:", recent_visits, flush=True)
+    print("count:", last_plate_count, flush=True)
+    if last_plate == result["plates"][0]["text"]:
+      last_plate_count += 1
+    else:
+      last_plate_count = 0
+    last_plate = result["plates"][0]["text"]
 
-  print(output)
-  cv2.imshow("VIDEO", frame)
-  cv2.waitKey(1)
+    if last_plate_count >= 30 and last_plate not in recent_visits.keys():
+      print("Adding plate!", flush=True)
+      t = time.time()
+      requests.post("http://backend:9000/api/visits", data = {'plate': last_plate, 'timestamp': t})
+      recent_visits[last_plate] = t
+
+
+
+
+  #print(output)
+  #cv2.imshow("VIDEO", frame)
+  #cv2.waitKey(1)
 
 ultimateAlprSdk.UltAlprSdkEngine_deInit()
