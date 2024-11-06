b853f0128aa6e40f9bc27577622df0c731631b8f
hanbeul <han310@gmail.com>
2024-11-06 16:22:11 -0500

    Added yolov5

--- yolo/main.py
+++ yolo/main.py
@@ -3,22 +3,23 @@ import cv2
 import numpy
 from flask import Flask, flash, request, redirect, url_for, make_response, jsonify
 
-# Model
 model = torch.hub.load("ultralytics/yolov5", "custom", path="./yolov5s_weights.pt", force_reload=True)  # or yolov5n - yolov5x6, custom
-
-# Images
-img = "image.png"  # or file, Path, PIL, OpenCV, numpy, list
-
-ALLOWED_EXTENSIONS = ['png']
 
 app = Flask(__name__)
 
-#@app.route("/")
+@app.route('/hello', methods=['GET'])
 def hello_world():
   results = model(img)
   crop = results.crop(save=False)[0]['im']
   results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
   return "<p>Hello lpdb!</p>"
+
+@app.route('/health', methods=['GET'])
+def health():
+  # results = model(img)
+  # crop = results.crop(save=False)[0]['im']
+  # results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+  return "{'healthy': 'yes'}"
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
